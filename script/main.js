6f4b2cde7c0a314c8acba628f5a8386ac22baba1
mahabubr <mahabub.r753@gmail.com>
2022-08-23 14:06:31 +0600

    add to title part complete

--- script/main.js
+++ script/main.js
@@ -1,5 +1,24 @@
+const titleName = [];
+
+function displayName() {
+    const addNameToCart = document.getElementById('name-cart');
+    addNameToCart.innerText = '';
+
+    for (let i = 0; i < titleName.length; i++) {
+        const span = document.createElement('span');
+        span.innerHTML = `
+        <h3>${i + 1}</h3>
+        <span class="side-card-title-name">${titleName[i]}</span>
+        `
+        addNameToCart.appendChild(span);
+    }
+
+}
+
 function onCLickBtn(element) {
     const titleNameElement = element.parentNode.parentNode.children[0];
     const titleNameElementText = titleNameElement.innerText;
-    console.log(titleNameElementText)
+    titleName.push(titleNameElementText)
+
+    displayName()
 }
