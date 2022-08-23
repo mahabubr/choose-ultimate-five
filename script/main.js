f7f5cd38d152c4cc856ad95522d464b015845d7c
mahabubr <mahabub.r753@gmail.com>
2022-08-23 13:45:31 +0600

    first commit

---
+++ script/main.js
@@ -0,0 +1,5 @@
+function onCLickBtn(element) {
+    const titleNameElement = element.parentNode.parentNode.children[0];
+    const titleNameElementText = titleNameElement.innerText;
+    console.log(titleNameElementText)
+}
